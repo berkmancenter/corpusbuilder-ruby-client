e1afb3b8f1d9bcd2cb8bcc757724137b8f8da3b5
Matt Galvin <mgalvin@endpoint.com>
2017-10-18 15:14:37 +0000

    Remove pre-set configuration

--- lib/corpusbuilder/ruby.rb
+++ lib/corpusbuilder/ruby.rb
@@ -3,6 +3,5 @@ require "corpusbuilder/ruby/version"
 module Corpusbuilder
   module Ruby
     include ActiveSupport::Configurable
-    Corpusbuilder::Ruby.config.base_url = "https://api.some.corpusbuilder.com/"
   end
 end
