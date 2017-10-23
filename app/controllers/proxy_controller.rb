97df597f129aadbc85a9c013bb7120d1ef2b8f0b
Matt Galvin <mgalvin@endpoint.com>
2017-10-23 10:40:10 -0400

    Update proxy controller

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -4,6 +4,6 @@ class ProxyController < ApplicationController
     http_verb = env["REQUEST_METHOD"].downcase
     headers = Corpusbuilder::Ruby::Api.new.get_headers
     url = Corpusbuilder::Ruby::Api.config.api_url + '/api' + request.original_url.split("corpusbuilder")[1]
-    RestClient.http_verb(url,params,headers)
+    RestClient::Request.execute(method: http_verb.to_sym, url: url, payload: params, headers: headers)
   end
 end
