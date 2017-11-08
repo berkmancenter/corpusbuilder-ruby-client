76414f4145a6f2c5cf38157bf411c50d1529907f
Kamil Ciemniewski <kamil@endpoint.com>
2017-11-08 12:27:49 +0000

    Made the proxy route return the actual JSON data back

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -4,6 +4,6 @@ class ProxyController < ApplicationController
     http_verb = env["REQUEST_METHOD"].downcase
     headers = Corpusbuilder::Ruby::Api.new.get_headers
     url = Corpusbuilder::Ruby::Api.config.api_url + '/api' + request.original_url.split("api")[1]
-    RestClient::Request.execute(method: http_verb.to_sym, url: url, payload: params, headers: headers)
+    render json: RestClient::Request.execute(method: http_verb.to_sym, url: url, payload: params, headers: headers).body
   end
 end
