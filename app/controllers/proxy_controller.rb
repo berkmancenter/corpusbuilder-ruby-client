aad24486e072468b465a191d09d91e82e2ca6820
Matt Galvin <mgalvin@endpoint.com>
2017-12-11 16:37:17 +0000

    Fix how json payloads are passed

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -4,6 +4,7 @@ class ProxyController < ApplicationController
     http_verb = env["REQUEST_METHOD"].downcase
     headers = Corpusbuilder::Ruby::Api.new.get_headers
     url = Corpusbuilder::Ruby::Api.config.api_url + '/api' + request.original_url.split("api")[1]
-    render json: RestClient::Request.execute(method: http_verb.to_sym, url: url, payload: params, headers: headers).body
+    payload = params.has_key?("_json") ? JSON.parse(params["_json"]) : params
+    render json: RestClient::Request.execute(method: http_verb.to_sym, url: url, payload: payload, headers: headers).body
   end
 end
