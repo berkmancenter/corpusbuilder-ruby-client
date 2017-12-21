15f42590dc3a4440937d6caecfbd2ac71de68c37
Kamil Ciemniewski <kamil@endpoint.com>
2017-12-21 14:09:34 +0000

    Refactored the api class and made the proxy controller inject the current editor

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -1,9 +1,43 @@
 class ProxyController < ApplicationController
 
   def speak_with_cb
-    http_verb = env["REQUEST_METHOD"].downcase
-    headers = Corpusbuilder::Ruby::Api.new.get_headers
+    result = {}
+    resulting_status = 200
+
     url = Corpusbuilder::Ruby::Api.config.api_url + '/api' + request.original_url.split("api")[1]
-    render json: RestClient::Request.execute(method: http_verb.to_sym, url: url, payload: params, headers: headers).body
+    payload = params.has_key?("_json") ? JSON.parse(params["_json"]) : params
+
+    begin
+      resp = RestClient::Request.execute(
+        method: env["REQUEST_METHOD"].downcase,
+        url: url,
+        payload: payload,
+        headers: proxy_headers
+      )
+      result = resp.body
+      resulting_status = resp.code
+    rescue => e
+      Rails.logger.error "#{e.message} - #{e.class.to_s}"
+      if e.respond_to?(:response)
+        result = e.response.body
+        resulting_status = e.response.code
+      else
+        resulting_status = 500
+      end
+    end
+
+    render json: result, status: resulting_status
   end
+
+  private
+
+  def proxy_headers
+    editor_id = Corpusbuilder::Ruby::Api.config.editor_id.call(self)
+    if editor_id.nil?
+      Corpusbuilder::Ruby::Api.new.headers
+    else
+      Corpusbuilder::Ruby::Api.new.headers.merge("X-Editor-Id" => editor_id)
+    end
+  end
+
 end
--- lib/corpusbuilder/models/api.rb
+++ lib/corpusbuilder/models/api.rb
@@ -6,79 +6,93 @@ module Corpusbuilder
     class Api
       include ActiveSupport::Configurable
 
-      def send_image(payload)
-        resp = RestClient.post(Corpusbuilder::Ruby::Api.config.api_url + "/api/images", payload, headers)
+      def get(url, _headers = headers)
+        resp = RestClient.get(Corpusbuilder::Ruby::Api.config.api_url + url, _headers)
         JSON.parse(resp.body)
+      end
+
+      def insert(url, payload, _headers = headers)
+        resp = RestClient.post(Corpusbuilder::Ruby::Api.config.api_url + url, payload, _headers)
+        JSON.parse(resp.body)
+      end
+
+      def update(url, payload, _headers = headers)
+        resp = RestClient.put(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents/#{document_id}/#{branch}/merge",
+                              payload,
+                              _headers)
+        JSON.parse(resp.body)
+      end
+
+      def send_image(payload)
+        insert("/api/images", payload)
       end
 
       def create_document(payload)
-        resp = RestClient.post(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents", payload, headers)
-        JSON.parse(resp.body)
+        insert("/api/documents", payload)
       end
 
       def get_documents
-        resp = RestClient.get(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents/", headers)
-        JSON.parse(resp.body)
+        get("/api/documents/")
       end
 
       def get_document(document_id)
-        resp = RestClient.get(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents/#{document_id}", headers)
-        JSON.parse(resp.body)
+        get("/api/documents/#{document_id}")
       end
 
       def get_document_status(document_id)
-        resp = RestClient.get(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents/#{document_id}/status", headers)
-        JSON.parse(resp.body)
+        get("/api/documents/#{document_id}/status")
       end
 
       def get_document_branches(document_id)
-        resp = RestClient.get(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents/#{document_id}/branches", headers)
-        JSON.parse(resp.body)
+        get("/api/documents/#{document_id}/branches")
       end
 
       def create_document_branch(document_id, editor_id, payload)
-        resp = RestClient.post(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents/#{document_id}/branches",
-                               payload,
-                               headers.merge!({ "X-Editor-Id" => editor_id })
-                              )
-        JSON.parse(resp.body)
+        insert(
+          "/api/documents/#{document_id}/branches",
+          payload,
+          headers.merge!({ "X-Editor-Id" => editor_id })
+        )
       end
 
       def merge_document_branches(document_id, branch, payload)
-        resp = RestClient.put(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents/#{document_id}/#{branch}/merge",
-                              payload,
-                              headers)
-        JSON.parse(resp.body)
+        update(
+          "/api/documents/#{document_id}/#{branch}/merge",
+          payload
+        )
       end
 
       def get_document_revision_tree(document_id, revision_id, payload={})
-        #There is a bug in RestClient which prevents the sending of params AND headers in a GET request, thus the params must be passed with the headers
+        # There is a bug in RestClient which prevents the sending of params
+        # AND headers in a GET request, thus the params must be passed with the headers
         # See RestCliet issue 397
-        resp = RestClient.get(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents/#{document_id}/#{revision_id}/tree", headers.merge!(:params => payload))
-        JSON.parse(resp.body)
+        get(
+          "/api/documents/#{document_id}/#{revision_id}/tree",
+          headers.merge!(:params => payload)
+        )
       end
 
       def get_document_revision_diff(document_id, revision_id, payload={})
-        #There is a bug in RestClient which prevents the sending of params AND headers in a GET request, thus the params must be passed with the headers
-        # See RestClient issue 397
-        resp = RestClient.get(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents/#{document_id}/#{revision_id}/diff", headers.merge!(:params => payload))
-        JSON.parse(resp.body)
+        # There is a bug in RestClient which prevents the sending of params
+        # AND headers in a GET request, thus the params must be passed with the headers
+        # See RestCliet issue 397
+        get(
+          "/api/documents/#{document_id}/#{revision_id}/diff",
+          headers.merge!(:params => payload)
+        )
       end
 
       def update_document_revision(document_id, revision_id, payload)
-        resp = RestClient.put(Corpusbuilder::Ruby::Api.config.api_url + "/api/documents/#{document_id}/#{revision_id}/tree", payload, headers)
-        JSON.parse(resp.body)
+        update(
+          "/api/documents/#{document_id}/#{revision_id}/tree",
+          payload
+        )
       end
 
       def create_editor(payload)
-        resp = RestClient.post(Corpusbuilder::Ruby::Api.config.api_url + "/api/editors", payload, headers)
+        insert("/api/editors", payload)
         JSON.parse(resp.body)
       end
-
-      def get_headers
-        headers
-      end
-      private
 
       def headers
         {
