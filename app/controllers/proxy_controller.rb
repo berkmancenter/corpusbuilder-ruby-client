c24df07c9679ad2673056a02dff3a118ce7b1bbb
Kamil Ciemniewski <kamil@endpoint.com>
2018-06-22 14:45:11 +0000

    Made the gem work well under Rails 5

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -7,7 +7,6 @@ class ProxyController < ApplicationController
     url = Corpusbuilder::Ruby::Api.config.api_url + '/api' + request.original_url.split("api")[1]
     payload = params.has_key?("_json") ? JSON.parse(params["_json"]) : params
 
-
     payload.delete "controller"
     payload.delete "action"
     payload.delete "path"
@@ -22,15 +21,15 @@ class ProxyController < ApplicationController
       }
 
       RestClient::Request.execute(
-        method: request.method.downcase,
+        method: request.method.downcase.to_sym,
         url: url,
-        payload: payload,
+        payload: payload.permit!.to_hash,
         headers: proxy_headers,
         timeout: 180,
         block_response: handle_response
       )
     rescue => e
-      Rails.logger.error "#{e.message} - #{e.class.to_s}"
+      Rails.logger.error "#{e.message} - #{e.class.to_s} - #{e.backtrace.join("\n")}"
       result = ""
       resulting_status = 500
 
